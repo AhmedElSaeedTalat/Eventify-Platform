5acbfcb8e3062620e409c491dd332635a20e2cd5
Oussama-hamdi <86133676+Oussama-hamdi@users.noreply.github.com>
2024-03-22 16:17:43 +0100

    Added Navbar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Home Page</h1>
       <Routes>
         <Route path="/login" element={<Login />} />
