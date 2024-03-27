f2cc341d4d6e5c4632c268d7b532a0a3b525bbc0
oussama007 <ohamdi776@gmail.com>
2024-03-27 14:28:51 +0000

    Refactored UserProfilePage component and applied style adjustments

---
+++ front-end/src/components/userProfileComponents/UserProfilePage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import SideBar from "./SideBar";
+import "./UserProfilePage.css";
+import UserProfile from "./UserProfile";
+
+const UserProfilePage = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const location = useLocation();
+
+  const isUserProfileRoute =
+    location.pathname === "/user-profile" ||
+    location.pathname === "/user-profile/";
+
+  return (
+    <div className="user-profile" style={{ height: "100vh" }}>
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          {/* Sidebar */}
+          <SideBar />
+
+          {/* Main content */}
+          <main className="col-md-9" style={{ overflowY: "auto" }}>
+            {isUserProfileRoute && <UserProfile />}
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfilePage;
