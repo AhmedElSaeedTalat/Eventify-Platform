cb0af5438f14fe84306dfa797681b74f51f98ca1
oussama007 <ohamdi776@gmail.com>
2024-03-30 14:42:48 +0000

    Add Redux integration for login and signup functionality, and include corresponding styles

--- front-end/src/components/Signup/Signup.js
+++ front-end/src/components/Signup/Signup.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess } from "../../reduxToolkit/slices/authSlice"; // Import the loginSuccess action
 import "./Signup.css";
 
 const SignupForm = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  console.log(isLoggedIn);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,65 +25,73 @@ const SignupForm = () => {
 
     try {
       // Make POST request to server to create new user
-      const response = await axios.post("/register", { email, password });
-      console.log(response.data); // Log response from server
+      // const response = await axios.post("/register", { email, password });
+      // console.log(response.data); // Log response from server
+
+      // Dispatch the loginSuccess action upon successful signup
+      dispatch(loginSuccess("Osama")); // Assuming response.data contains user information
+
       // Reset form fields after successful submission
       setEmail("");
       setPassword("");
       setConfirmPassword("");
       setError("");
+
+      navigate("/");
     } catch (error) {
       console.error("Error creating user:", error);
     }
   };
 
   return (
-    <div className="signup-form">
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="confirmPassword" className="form-label">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Signup
-        </button>
-      </form>
+    <div className="auth-form container">
+      <div className="signup-form">
+        <h2>Signup</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="confirmPassword" className="form-label">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Signup
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
