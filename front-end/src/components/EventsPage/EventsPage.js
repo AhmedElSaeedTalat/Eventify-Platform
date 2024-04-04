e8ada001dfa41b67a8f756787a1eba3ddf351548
oussama007 <ohamdi776@gmail.com>
2024-04-04 11:20:35 +0000

    Update eventsPage & CreateEvent components

--- front-end/src/components/EventsPage/EventsPage.js
+++ front-end/src/components/EventsPage/EventsPage.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import EventCard from "../EventCard/EventCard";
 import Pagination from "./Pagination";
-import { fetchEvents, deleteEvent } from "../../reduxToolkit/slices/EventSlice";
+import { fetchEvents } from "../../reduxToolkit/slices/EventSlice";
 import "./EventsPage.css";
 
 const EventsPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const eventsPerPage = 3; // Number of events per page
+  const eventsPerPage = 3;
 
   const dispatch = useDispatch();
 
   // Fetch events data from Redux store
   const { events, status, error } = useSelector((state) => state.events);
+  console.log("Events: ", events);
 
   useEffect(() => {
     dispatch(fetchEvents());
@@ -23,10 +24,13 @@ const EventsPage = () => {
     setCurrentPage(pageNumber);
   };
 
+  // Filter events with Active state
+  const activeEvents = events.filter((event) => event.state === "Active");
+
   // Get current events based on pagination
   const indexOfLastEvent = currentPage * eventsPerPage;
   const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
-  const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
+  const currentEvents = activeEvents.slice(indexOfFirstEvent, indexOfLastEvent);
 
   return (
     <div className="container events-page">
@@ -60,7 +64,7 @@ const EventsPage = () => {
       {/* Render pagination */}
       <Pagination
         currentPage={currentPage}
-        totalPages={Math.ceil(events.length / eventsPerPage)}
+        totalPages={Math.ceil(activeEvents.length / eventsPerPage)}
         onPageChange={handlePageChange}
       />
     </div>
