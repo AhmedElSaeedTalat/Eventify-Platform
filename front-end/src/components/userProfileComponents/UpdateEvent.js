6757e361d93c9a4b3109ce13605a783ec44189a5
oussama007 <ohamdi776@gmail.com>
2024-04-04 15:57:23 +0000

    Update updateEvent to fetch API

--- front-end/src/components/userProfileComponents/UpdateEvent.js
+++ front-end/src/components/userProfileComponents/UpdateEvent.js
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-const UpdateEvent = ({ eventData }) => {
-  const [updatedEventData, setUpdatedEventData] = useState({ ...eventData });
+const UpdateEvent = () => {
+  const { eventId } = useParams(); // Get eventId from URL params
+  const [updatedEventData, setUpdatedEventData] = useState({}); // Initialize with an empty object
   const [imageFile, setImageFile] = useState(null);
+
+  // Update state with eventData when it changes
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5001/event/${eventId}`
+        );
+        setUpdatedEventData(response.data);
+        console.log("Event data:", response.data);
+      } catch (error) {
+        console.error("Error fetching event:", error);
+      }
+    };
+
+    fetchData();
+  }, [eventId]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUpdatedEventData((prevData) => ({
       ...prevData,
-      [name]: name === "price" ? parseFloat(value) : value, // Convert price to number
+      [name]: name === "price" ? parseFloat(value) : value,
     }));
   };
 
@@ -19,27 +39,65 @@ const UpdateEvent = ({ eventData }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Perform any action with the updatedEventData, such as sending it to a server
     console.log("Updated event data submitted:", updatedEventData);
   };
 
   const minDate = new Date();
-  minDate.setDate(minDate.getDate() + 1); // Add one day to today's date
+  minDate.setDate(minDate.getDate() + 1);
+
+  const categoryNames = [
+    "Any category",
+    "New Groups",
+    "Art & Culture",
+    "Career & Business",
+    "Community & Environment",
+    "Dancing",
+    "Games",
+    "Health & Wellbeing",
+    "Hobbies & Passions",
+    "Identity & Language",
+    "Movements & Politics",
+    "Music",
+    "Parents & Family",
+    "Pets & Animals",
+    "Religion & Spirituality",
+    "Science & Education",
+    "Social Activities",
+    "Sports & Fitness",
+    "Support & Coaching",
+    "Technology",
+    "Travel & Outdoor",
+    "Writing",
+    "Weddings",
+  ];
 
   return (
     <div className="container update-event">
       <h2 className="mt-4 mb-3">Update Event</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="title" className="form-label">
-            Title:
+          <label htmlFor="name" className="form-label">
+            Name:
           </label>
           <input
             type="text"
             className="form-control"
-            id="title"
-            name="title"
-            value={updatedEventData.title}
+            id="name"
+            name="name"
+            value={updatedEventData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description:
+          </label>
+          <textarea
+            className="form-control"
+            id="description"
+            name="description"
+            value={updatedEventData.description}
             onChange={handleChange}
             required
           />
@@ -60,6 +118,22 @@ const UpdateEvent = ({ eventData }) => {
           />
         </div>
         <div className="mb-3">
+          <label htmlFor="state" className="form-label">
+            State:
+          </label>
+          <select
+            className="form-select"
+            id="state"
+            name="state"
+            value={updatedEventData.state}
+            onChange={handleChange}
+            required
+          >
+            <option value="Active">Active</option>
+            <option value="Cancelled">Cancelled</option>
+          </select>
+        </div>
+        <div className="mb-3">
           <label htmlFor="location" className="form-label">
             Location:
           </label>
@@ -74,27 +148,48 @@ const UpdateEvent = ({ eventData }) => {
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="description" className="form-label">
-            Description:
+          <label htmlFor="organizer" className="form-label">
+            Organizer:
           </label>
-          <textarea
+          <input
+            type="text"
             className="form-control"
-            id="description"
-            name="description"
-            value={updatedEventData.description}
+            id="organizer"
+            name="organizer"
+            value={updatedEventData.organizer}
             onChange={handleChange}
             required
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="imageSrc" className="form-label">
-            Upload Image:
+          <label htmlFor="category" className="form-label">
+            Category:
+          </label>
+          <select
+            className="form-select"
+            id="category"
+            name="category"
+            value={updatedEventData.category}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select category...</option>
+            {categoryNames.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="image" className="form-label">
+            Image:
           </label>
           <input
             type="file"
             className="form-control"
-            id="imageSrc"
-            name="imageSrc"
+            id="image"
+            name="image"
             accept="image/*"
             onChange={handleImageUpload}
           />
